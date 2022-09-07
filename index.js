87db35b24f5ed4bd287be5e1d10c623f0ff37f57
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2022-09-07 23:44:39 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const multer = require('multer')
 require('dotenv').config()
 
 const app = express();
@@ -9,13 +10,22 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+// app.use(express.bodyParser())
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let upload = multer({ dest: 'uploads/' })
 
+app.post('/api/fileanalyse',upload.single('upfile'),function (req,res){
+  let result = {
+    name : req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size
+  }
+  res.send(result)
+})
 
 
 const port = process.env.PORT || 3000;
